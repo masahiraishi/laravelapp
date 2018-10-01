7167322f66963ac941852f1ae7d18d43997d523a
平石匡 <masahiraishi22@gmail.com>
2018-10-01 21:56:28 +0900

    テンプレートの変更

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,5 +1,4 @@
 
-
 @extends('layouts.helloapp')
 
 @section('title',Index)
@@ -18,37 +17,3 @@
 copylight 2017
 @endsection
 
-
-
-
-
-
-
-
-
-
-<html>
-<head>
-	<title>Hello/Index</title>
-	<style>
-	body { font-size:16pt;color:#999; }
-	h1 { font-size:50px; text-align:right; color:#f6f6f6;
-		margin:-20px 0px -30px 0px; letter-spacing:-4px;}
-	</style>
-</head>
-<body>
-	<h1>Blade/Index</h1>
-	<p>&#064;foreach ディレクティブの例</p>
-	<ol>
-	@php
-	$counter = 0;
-	@endphp
-	@while ($counter< count($data))
-	<li>{{$data[$counter]}}</li>
-	@php
-	$couunter++;
-	@endphp
-	@endwhile
-	</ol>
-</body>
-</html>
