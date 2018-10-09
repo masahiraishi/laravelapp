5578db1d4406fea5dae80c1d16df88d2f710de2e
平石匡 <masahiraishi22@gmail.com>
2018-10-09 23:53:41 +0900

    バリデーションの設定

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,13 +10,19 @@ class HelloController extends Controller
 {
 	public function index (Request $request){
 
-		return view('hello.index');
+		return view('hello.index',['msg'=>'フォーム入力:']);
 
 	}
 
 
-	public function post(Request $request){
-
+	public function post(Request $request)
+    {
+        $validate_rule =[
+            'name'=>'required',
+            'mail'=>'email',
+            'age'=>'numeric|between:0,150',
+        ];
+        $this->validate($request,$validate_rule);
 		return view('hello.index',['msg'=>$request->msg]);
 
 	}
