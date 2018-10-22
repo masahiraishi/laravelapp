c891517fae9a1498fb02e7257d84001481aaacc6
平石匡 <masahiraishi22@gmail.com>
2018-10-23 07:52:10 +0900

    db select

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,19 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
 	public function index (Request $request)
     {
-        if($request->hasCookie('msg'))
-        {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        }else{
-            $msg = '※クッキーはありません';
-        }
+        $items = DB::select('select * from people');
 
-		return view('hello.index',['msg'=>$msg]);
+        return view('hello.index',['items'=>$items]);
 	}
 
 
