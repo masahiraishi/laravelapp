f1239f0719127038521225b666cb2ebb4e5acc04
平石匡 <masahiraishi22@gmail.com>
2018-10-24 06:00:40 +0900

    dbインサート

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,6 +12,7 @@ class HelloController extends Controller
 {
 	public function index (Request $request)
     {
+
         $items = DB::select('select * from people');
 
         return view('hello.index',['items'=>$items]);
@@ -20,16 +21,25 @@ class HelloController extends Controller
 
 	public function post(Request $request)
     {
-        $validate_rule = [
-            'msg' =>'required',
-        ];
+       $items = DB::select('select * FROM people');
 
-        $this->validate($request,$validate_rule);
-        $msg = $request->msg;
-        $response = new Response(view('hello.index',['msg'=>'「'.$msg.'」をクッキーに保存しました。' ]));
-        $response->cookie('msg',$msg,100);
-
-		return $response;
+       return view ('hello.index',['items' =>$items]);
 	}
+
+	public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' =>$request->name,
+            'mail' =>$request->mail,
+            'age' =>$request->age,
+        ];
+        DB::insert('insert into people (name,mail,age) values (:name,:mail,:age)',$param);
+        return redirect('/hello');
+    }
 
 }
