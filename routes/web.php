2e1817fdd07242991734e6bcb529fe448c9d300b
平石匡 <masahiraishi22@gmail.com>
2018-10-25 00:10:38 +0900

    edit機能

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::post('hello','HelloController@post');
 Route::get('hello/add','HelloController@add');
 Route::post('hello/add','HelloController@create');
 
+Route::get('hello/edit','HelloController@edit');
+Route::post('hello/edit','HelloController@update');
+
