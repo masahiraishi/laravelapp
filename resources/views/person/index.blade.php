817037a603da975951a0439e1f7be56922e8d731
平石匡 <masahiraishi22@gmail.com>
2018-10-28 16:26:17 +0900

    Eloquentコミット

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.helloapp')
+
+@section('title','Person.index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <table>
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach($items as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
+
+@section('footer')
+    copyright 2017 tuyano.
+@endsection
+
