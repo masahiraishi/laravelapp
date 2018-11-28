1ad79d68eba9112785fe868aad3bb2356a94379b
平石匡 <masahiraishi22@gmail.com>
2018-11-28 21:58:36 +0900

    restモデル系コミット

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,5 @@ Route::get('board','BoardController@index');
 Route::get('board/add','BoardController@add');
 Route::post('board/add','BoardController@create');
 
+Route::resource('rest','RestappController');
+
